afa3c7fe5d5cd678e247d28c1ab88736e5f672b9
user <github@mukaseev.ru>
2025-09-02 16:03:13 +0300

    update .gitignore

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+import telebot
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    raise RuntimeError("Нет TOKEN в .env")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я живой 🖐️\nКоманды: /start, /help")
+
+@bot.message_handler(commands=['help'])
+def help_cmd(message):
+    bot.reply_to(message, "/start — начать, /help — подсказка")
+
+if __name__ == "__main__":
+    bot.infinity_polling(skip_pending=True)
